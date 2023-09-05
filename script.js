2bb7c1648ba727ab609ae1c911f033cc6c4cfb84
Chirag Mittal <118048068+chiragmittal36269@users.noreply.github.com>
2023-09-05 20:56:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function decimalToBinary(num) {
+function decimalToBinary(decimalNum) {
   //Write you code here
+	 if (decimalNum === 0) {
+        return '0';
+    }
+
+    let binaryNum = '';
+    while (decimalNum > 0) {
+        const remainder = decimalNum % 2;
+        binaryNum = remainder + binaryNum;
+        decimalNum = Math.floor(decimalNum / 2);
+    }
+
+    return binaryNum;
   
 }
 
